5ace5525c89d1cb59856988bd885fc48cc67f445
Viktor Yushkevich <viktor.yushkevich@vrpconsulting.com>
2020-09-01 22:53:34 +0300

    feat: newcommit2

---
+++ build/precache-manifest.06f4179270cf04707cee784122918795.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510919c84088eee739e1d5b3f77a2c42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1673ad6413d33858716",
+    "url": "/static/css/2.7325e055.chunk.css"
+  },
+  {
+    "revision": "6559495dd64c3737fc90",
+    "url": "/static/css/main.75f0dd73.chunk.css"
+  },
+  {
+    "revision": "b1673ad6413d33858716",
+    "url": "/static/js/2.6a39a4de.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6a39a4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6559495dd64c3737fc90",
+    "url": "/static/js/main.58aefafa.chunk.js"
+  },
+  {
+    "revision": "8146f0cabe9c8f6aeb56",
+    "url": "/static/js/runtime-main.cad64008.js"
+  },
+  {
+    "revision": "bc7e95be5983bf0db86adbd23f95f0e9",
+    "url": "/static/media/logo-1.bc7e95be.png"
+  }
+]);
